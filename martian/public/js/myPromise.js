fe6832f80cda69a2a528b6de062110393ed25b02
Hannah Park <ha3158987@gmail.com>
2021-03-03 21:51:48 +0900

    Feat: make converToHexadecimal func

--- martian/public/js/myPromise.js
+++ martian/public/js/myPromise.js
@@ -3,19 +3,21 @@
 /* --------------------------------------------------------------------- */
 
 /*
-! then은 콜백함수를 인자로 받으며 자기 자신(this)을 리턴한다.
-TODO: - [ ] MyPrimise 안에서 순서대로 실행되어야 하는 함수는 cblist라는 배열에 담는다.
-TODO: - [ ]
-
+! then은 콜백함수를 인자로 받으며 자기 자신(this/MyPromise)을 리턴하며, 동기로 실행된다.
+! cbList내부의 콜백함수들을 차례대로 실행하려면, cbList에 추가하는 로직은 동기로 실행해야한다.
+TODO - [ ] MyPromise 안에서 순서대로 실행되어야 하는 함수는 cblist라는 배열에 담는다.
 */
 
+//미완성
 export default class MyPromise {
     constructor(callback) {
         this.cbList = [];
-        this.then(this.cb);
+        // setTimeout(this.cbList.forEach(fn) => fn());
+        this.then(callback);
     }
 
-    then(cb){
+    then(callback){
+        this.cbList.push(callback)
         return this;
     }
-} 
+}
