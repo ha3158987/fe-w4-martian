8763b9c0a72ab65e519c8c099976c29ccccba6f2
Hannah Park <ha3158987@gmail.com>
2021-03-06 03:53:08 +0900

    Feat: add func to send words to Mars asynchronously

--- martian/public/js/app.js
+++ martian/public/js/app.js
@@ -31,20 +31,42 @@ function activateTranslator(textList){
 
 //5초마다 지구어 보내기
 function sendSignalToMars(strArr) {
+    let wholeStr = '';
     strArr.forEach((el, i) => {
-        setTimeout(() => showReceivedWord(el), (i + 1) * 1000);
+        wholeStr += ` ${el}`;
+        setTimeout(() => sendWord(el, wholeStr), (i + 1) * 1000);
     })
 }
 
+//배열로 문자열을 쪼개고, 반복문을 돌리기
+//한 글자마다 실행되어야 할 함수: 화살표 돌리기, 받은 글자 viewBox에 보여주기
+const splitString = (str) => str.split('');
+
 //받은 지구어 16진수 아스키코드로 변환해서 출력
-function showReceivedWord(str) {
+function sendWord(str, wholeStr) {
+    const viewBox = DOMREF.resultBox;
     const receivedWord = convertToHexadecimal(str);
-    console.log(str);
-    console.log(receivedWord);
+    receivedWord.forEach(letterPair => {
+        const oneLetterArr = splitString(letterPair); //결과:["6", "8"]
+        oneLetterArr.forEach(letter => {
+            
+        })
+        console.log("oneLetter", oneLetterArr);
+    })
+
+    // viewBox.innerText = wholeStr; //이건 나중에 '해석하기'버튼이 눌리면 보여져야 함.
+    
+    viewBox.innerText = receivedWord;
+
+    console.log("str:", str);
+    console.log("receivedWord", receivedWord);
+    console.log(wholeStr);
 }
 
+
+
 (function init(){
-    sendSignalToMars(["hello", "mars", "howalive?"]);
+    sendSignalToMars(["hello", "mars"]);
     activateTranslator(DOMREF.textList);
     console.log(DOMREF.textList)
 })();
