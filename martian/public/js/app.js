8aa73083cb5f46cbad8b6795bf21b0be4ea73940
Hannah Park <ha3158987@gmail.com>
2021-03-05 21:57:29 +0900

    Feat: make sendSignalToMars & showReceivedWord func

--- martian/public/js/app.js
+++ martian/public/js/app.js
@@ -6,36 +6,47 @@
 ! Dom 핸들링은 여기서한다. translator.js는 오로지 로직에 관련된 것만 갖게 할 것.
 todo - [x] translator 돌림판 위의 text node를 배열에 담아오는 함수 만들기
 todo - [x] IIFE함수로 자동시작시키는 함수 만들기
-todo - [ ] setTimeout으로 미리 입력된 text를 5초에 한번씩 화성으로 보내는 함수 만들기. (발송 되기 전 지구어를 아스키코드로 먼저 변환한다.)
+todo - [x] setTimeout으로 미리 입력된 text를 5초에 한번씩 화성으로 보내는 함수 만들기. (발송 되기 전 지구어를 아스키코드로 먼저 변환한다.)
 todo - [ ] '해석하기', '지구로 보내기'버튼에 클릭이벤트 걸어주기
 todo - [ ] MyPromise 생성자 미완 - 완성 시 활용하는 방법으로 리팩토링할 것.
 */
 
 // import MyPromise from "./myPromise.js";
-import { convertToHexadecimal } from "./translator.js";
+import { convertToHexadecimal , convertToString } from "./translator.js";
 import utill from "./utill.js";
+const _ = utill;
 
 const DOMREF = {
-    textList : utill.$all("path"),
-    hexToStrBtn : utill.$(".hexadecimal_translate"),
-    sendToEarthBtn : utill.$(".languag_send"),
-    sendInput: utill.$(".language_input")
+    textList : _.$all("path"),
+    hexToStrBtn : _.$(".hexadecimal_translate"),
+    resultBox: _.$(".hexadecimal_result"),
+    sendToEarthBtn : _.$(".languag_send"),
+    sendInput: _.$(".language_input")
 }
 
-//변환기 구동시키기 = text node를 가져오기
+//변환기 구동시키기 = text node를 가져오기/화살표 이동거리 계산하기/화살표 움직이기/해석하기 버튼 활성화/해석결과 보여주기
 function activateTranslator(textList){
-    return getNodeList(textList);
+    return textList;
 }
 
 //5초마다 지구어 보내기
 function sendSignalToMars(strArr) {
-    strArr.
-    setTimeout()
+    strArr.forEach((el, i) => {
+        setTimeout(() => showReceivedWord(el), (i + 1) * 1000);
+    })
+}
+
+//받은 지구어 16진수 아스키코드로 변환해서 출력
+function showReceivedWord(str) {
+    const receivedWord = convertToHexadecimal(str);
+    console.log(str);
+    console.log(receivedWord);
 }
 
 (function init(){
     sendSignalToMars(["hello", "mars", "howalive?"]);
     activateTranslator(DOMREF.textList);
+    console.log(DOMREF.textList)
 })();
 
 // const MYPROMISE = new MyPromise();
--- martian/public/js/translator.js
+++ martian/public/js/translator.js
@@ -8,7 +8,7 @@ todo - [x] 16진수 -> 문자열 변환 함수 만들기
 todo - [x] 문자열 -> 16진수(배열) 변환 함수 만들기
 todo - [ ] 수신한 16진수를 송수신정보에 출력해주는 함수 만들기
 todo - [ ] 해석하기 버튼에 클릭이벤트 발생 시 지구어로 변환된 결과를 송수신정보에 출력해주는 함수 만들기
-todo - [ ] 각 16진수 글자와 각 16진수 글자에 해당하는 각도(화살표가 회전해야 하는 각도)를 key-value로 저장하기(현재 화살표 위치가 어디냐에 따라 달라지기 때문에 해당이 안됌. 다른 방법 생각할 것)
+todo - [ ] 현재 화살표 위치에서 다음 글자를 가리킬때 이동각도 계산해주는 함수 만들기
 todo - [ ] 수신한/입력받은 16진수를 2초마다 하나씩 가리키는 함수(화살표의 각도를 돌려주는 애니메이션 함수) 만들기
 */
 
@@ -28,9 +28,8 @@ function convertToString (hexArr) {
     .reduce((acc, dec) => acc += String.fromCharCode(dec), '');
 }
 
-
 //test code
-console.log(convertToHexadecimal('howalive?'));  //["68", "6f", "77", "61", "6c", "69", "76", "65", "3f"]
-console.log(convertToString(["68", "65", "6c", "6c", "6f"])); //hello
+// console.log(convertToHexadecimal('howalive?'));  //["68", "6f", "77", "61", "6c", "69", "76", "65", "3f"]
+// console.log(convertToString(["68", "65", "6c", "6c", "6f"])); //hello
 
-export { convertToHexadecimal }
+export { convertToHexadecimal, convertToString }
