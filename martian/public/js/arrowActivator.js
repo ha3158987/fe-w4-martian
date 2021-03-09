4c11cef0159bbcf76a9b6cc915dcecf1ed99abd0
Hannah Park <ha3158987@gmail.com>
2021-03-10 00:42:39 +0900

    Feat: add arrow-moving effect on every incoming letter

--- martian/public/js/arrowActivator.js
+++ martian/public/js/arrowActivator.js
@@ -1,30 +1,26 @@
-// import DOMREF from "./app.js";
 import _ from "./utill.js";
 
-const isFirst = (node) => _.contains(node, "first");
-
-function checkFirst(buttonNode) {
-    if (isFirst(buttonNode)) _.removeClass(buttonNode, "first");
-    return isFirst(buttonNode) ? ((360 / 16) / 2) : 0;
-}
-
-function calculateDegree(letter, startingPoint) {
-    const degreePerLetter = 360 / 16;
-    const decimal = parseInt(letter, 16); //10진수
-    let targetDegree = decimal * degreePerLetter;
-    startingPoint === 0 ? null : targetDegree += startingPoint;
-    return targetDegree;
+function calculateDeg(letter) {
+    const degPerLetter = 360 / 16;
+    const startingPoint = degPerLetter / 2;
+    const decimal = parseInt(letter, 16);
+    let targetDeg = decimal * degPerLetter + startingPoint;
+    return targetDeg;
 }
 
 function getPrevArc(arrowNode) {
     const style = window.getComputedStyle(arrowNode);
-    const prevArc = style.getPropertyValue('transform');
+    return style.getPropertyValue('transform');
+}
+
+function moveArrow(arrow, targetDeg, arrowLocation) {
+    arrow.style.transform = `rotate(${targetDeg}deg)`;
 }
 
 function rotateArrow(letter, translateButton, arrow) {
-    const startingPoint = checkFirst(translateButton);
-    const targetDegree = calculateDegree(letter, startingPoint);
-    const prevArc = getPrevArc(arrow);
+    const targetDeg = calculateDeg(letter);
+    const arrowLocation = getPrevArc(arrow);
+    moveArrow(arrow, targetDeg, arrowLocation);
 }
 
 export default rotateArrow;
--- martian/public/js/translator.js
+++ martian/public/js/translator.js
@@ -6,14 +6,7 @@
 ! 모든 함수는 순수하게(의존성을 낮추기) 만들기. 매직넘버 없게 주의.
 todo - [x] 16진수 -> 문자열 변환 함수 만들기
 todo - [x] 문자열 -> 16진수(배열) 변환 함수 만들기
-todo - [ ] 수신한 16진수를 송수신정보에 출력해주는 함수 만들기
-todo - [ ] 해석하기 버튼에 클릭이벤트 발생 시 지구어로 변환된 결과를 송수신정보에 출력해주는 함수 만들기
-todo - [ ] 현재 화살표 위치에서 다음 글자를 가리킬때 이동각도 계산해주는 함수 만들기
-todo - [ ] 수신한/입력받은 16진수를 2초마다 하나씩 가리키는 함수(화살표의 각도를 돌려주는 애니메이션 함수) 만들기
-todo - [ ] 진수 변환기 pipe함수로 리팩토링하기
 */
-
-import utill from './utill.js';
 
 //문자열 -> 10진수 -> 16진수 (배열로 return)
 function convertToHexadecimal (letterArr) {
@@ -27,9 +20,5 @@ function convertToString (hexArr) {
     .map(hex => parseInt(hex, 16))
     .reduce((acc, dec) => acc += String.fromCharCode(dec), '');
 }
-
-//test code
-// console.log(convertToHexadecimal('howalive?'));  //["68", "6f", "77", "61", "6c", "69", "76", "65", "3f"]
-// console.log(convertToString(["68", "65", "6c", "6c", "6f"])); //hello
 
 export { convertToHexadecimal, convertToString }
