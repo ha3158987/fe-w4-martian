d55ddfb007c5e37b0d93544127b5c67953443031
Hannah Park <ha3158987@gmail.com>
2021-03-09 21:33:46 +0900

    Feat: create arrowActivator module & add calculateDegree func

--- martian/public/js/app.js
+++ martian/public/js/app.js
@@ -3,25 +3,17 @@
 /* --------------------------------------------------------------------- */
 
 /*
-! 
-! Dom 핸들링은 여기서한다. translator.js는 오로지 로직에 관련된 것만 갖게 할 것.
-todo - [x] translator 돌림판 위의 text node를 배열에 담아오는 함수 만들기
-todo - [x] IIFE함수로 자동시작시키는 함수 만들기
-todo - [x] setTimeout으로 미리 입력된 text를 5초에 한번씩 화성으로 보내는 함수 만들기. (발송 되기 전 지구어를 아스키코드로 먼저 변환한다.)
-        * ------------세부사항-------------
-        * 1. 5초 간격으로 단어를 하나씩 보낸다. sendSignalToMars (x)
-        * 2. 하나의 알파벳 단위로 단어를 쪼갠다. splitString (x)
-        * 3. 쪼개진 알파벳은 16진수 아스키코드로 변환해서 한 글자씩 수신기로 보낸다. sendHexOneByOne (x)
-todo - [ ] (수신기 입장에서) 글자 1개가 들어온면 일어나는 일:
-        * ------------세부사항-------------
-        * 1. 화살표가 해당 16진수 글자를 "2초동안" 가리켜야 한다.
-        * 2. 송수신정보에 해당 글자를 출력(render)해줘야한다. 이 때, 2글자가 들어오면 ''(띄어쓰기)를 추가해서 글자간 간격을 넣어준다. 그리고 ,(콤마)는 없애준다.
-todo - [ ] '해석하기', '지구로 보내기'버튼에 클릭이벤트 걸어주기
-todo - [ ] MyPromise 생성자 미완 - 완성 시 활용하는 방법으로 리팩토링할 것.
+todo [x] 지구로부터 string을 받는다 -> 받은 string을 split해준다 -> 16진수로 변환한다.
+todo [x] 한 쌍의 16진수를 다시 하나의 글자로 쪼개서 반복문을 돌린다. 한 글자가 반복문에 들어올 때마다 실행되어야 하는 일들:
+    1) 화살표 움직이기
+    2) (화살표 움직임이 끝나면)textbox에 받은 글자 누적해서 보여주기
+    3) 해당 글씨 반짝이기
+    4) (2번이 끝나면)해석하기버튼 활성화하기
 */
 
 // import MyPromise from "./myPromise.js";
 import { convertToHexadecimal , convertToString } from "./translator.js";
+import rotateArrow from "./arrowActivator.js";
 import utill from "./utill.js";
 const _ = utill;
 
@@ -46,7 +38,7 @@ function analyzeWord(str) {
 async function getHexOneByOne(hex){
     for (let oneHex of hex) {
         activateTranslator(oneHex);
-        await delay(1000);
+        await delay(2000);
     }
 }
 
@@ -54,7 +46,7 @@ async function getWordFromEarth(str) {
     const hexArr = analyzeWord(str);
     for (let hex of hexArr) {
         getHexOneByOne(hex);
-        await delay(2000);
+        await delay(5000);
     }
 }
 
@@ -67,10 +59,11 @@ function fillUpTextBox(str) {
 //변환기 구동시키기 = text node를 가져오기/화살표 이동거리 계산하기/화살표 움직이기/해석하기 버튼 활성화/해석결과 보여주기
 function activateTranslator(letter){
     fillUpTextBox(letter);
-
+    rotateArrow(letter);
 }
-
 
 (function init(){
     getWordFromEarth("howalive");
 })();
+
+export default DOMREF;
